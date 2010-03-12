167c1786d9a2a8c3f71cb1abb047f9eac6d32c51
Richard Hart <richard@ur-ban.com>
2010-03-12 23:07:02 +0000

    Adding readme.

--- lib/git_acp.rb
+++ lib/git_acp.rb
@@ -5,21 +5,24 @@ class GitACP
   end
 
   def add_commit_push
-    puts "Pushing to #{current_branch}"
+    puts "Pushing to #{current_branch}."
     add
     commit
     push
   end
 
   def add
+    puts "Running add."
     `git add .`
   end
 
   def commit
-    `git commit -am #{@args[0]}`
+    puts "Running commit with message: #{@args[0]}"
+    `git commit -am "#{@args[0]}"`
   end
 
   def push
+    puts "Running push."
     `git push origin #{current_branch}`
   end
 

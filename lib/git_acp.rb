8607799c2817fde51ee9989cc75295b01eb7e6ce
Richard Hart <richard@ur-ban.com>
2010-03-12 22:22:20 +0000

    Testing

--- lib/git_acp.rb
+++ lib/git_acp.rb
@@ -1,5 +1,28 @@
 class GitACP
 
+  def initialize args
+    @args = args
+  end
+
+  def add_commit_push
+    puts "Pushing to #{@args[0]}"
+    add
+    commit
+    push
+  end
+
+  def add
+    `git add .`
+  end
+
+  def commit
+    `git commit -am #{@args[0]}`
+  end
+
+  def push
+    `git push origin #{current_branch}`
+  end
+
   def current_branch
     `git branch --no-color`.match(/\*\s(.+?)\s/)[1]
   end
